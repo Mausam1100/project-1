449d3d4d5e9fec192d19b23476f08944a13b6039
Mausam Baduwal <mausambaduwal303@gmail.com>
2024-07-14 22:42:51 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import TextArea from './components/TextArea.js';
+import React, {useState} from 'react'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [navBg, setNavBg] = useState("bg-blue-200 text-black")
+  const [modeBtn, setModeBtn] = useState("bg-slate-800 text-white")
+  const [functionBtn, setFunctionBtn] = useState("bg-blue-500 text-white")
+
+  const toggleMode = () => {
+    if(!darkMode) {
+      setNavBg("bg-slate-800 text-white")
+      setModeBtn("bg-blue-200 text-black")
+      document.body.style.backgroundColor = "rgb(100 116 139)"
+      setFunctionBtn("bg-white text-black")
+      setDarkMode(true)
+    }
+    else {
+      setNavBg("bg-blue-200 text-black")
+      setModeBtn("bg-slate-800 text-white")
+      document.body.style.backgroundColor = "white"
+      setFunctionBtn("bg-blue-500 text-white")
+      setDarkMode(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar navBg={navBg} toggleMode={toggleMode} modeBtn={modeBtn} title="WordPress"/>
+      <TextArea functionBtn={functionBtn} darkMode={darkMode}/>
+    </>
   );
 }
 
